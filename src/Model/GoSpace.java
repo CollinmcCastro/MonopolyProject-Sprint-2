3b084126877426b8fc32b14b987715af89d73d61
collinmccastro <collinmcc04@gmail.com>
2025-03-27 00:06:20 -0400

    Changed all player.increase and player.decrease calls to bank.payplayer and bank.collectfrom player.
    Also took the 200 when you pass go functionality out of Gameboard.java and refactored the call in GoSpace.java

--- src/Model/GoSpace.java
+++ src/Model/GoSpace.java
@@ -5,9 +5,15 @@ public class GoSpace extends Space {
         super("Go");
     }
 
+    private Bank bank;
+
+    public void setBank(Bank bank) {
+        this.bank = bank;
+    }
+
     @Override
     public void landOn(Player player) {
-        player.increaseMoney(200); // Collect $200 for passing Go
+        bank.payPlayer(player,200); // Collect $200 for passing Go
         System.out.println(player.getName() + " landed on Go and collected $200.");
     }
 }
